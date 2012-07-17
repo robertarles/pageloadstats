cf4115159fac6cb45bed9b451c4b464364ad9fc7
robert <robert@arles.us>
2012-07-17 12:21:46 -0700

    added names to admin panel for target_alert associations.

--- PageLoadStatsPy/pageloadstats/admin.py
+++ PageLoadStatsPy/pageloadstats/admin.py
@@ -1,6 +1,27 @@
-from PageLoadStatsPy.pageloadstats.models import Alert, Target, Stat
+from PageLoadStatsPy.pageloadstats.models import Alert, Target, Stat, AlertAlertRecipients, AlertRecipients, TargetAlert
 from django.contrib import admin
 
-admin.site.register(Target)
+
+class TargetAdmin(admin.ModelAdmin):
+    list_display = ('id','name','url')
+
+
+class AlertAdmin(admin.ModelAdmin):    
+    list_display = ("id", "name", "limit_high")
+
+
+class TargetAlertAdmin(admin.ModelAdmin):
+    def target_name(self, target_alert):
+        return Target.objects.get(id=target_alert.target_id).name
+    def alert_name(self, target_alert):
+        return Alert.objects.get(id=target_alert.alert_id).name
+    
+    list_display = ("id","target_name", "alert_name")
+    
+    
+admin.site.register(Target, TargetAdmin)
 admin.site.register(Stat)
-admin.site.register(Alert)
+admin.site.register(Alert, AlertAdmin)
+admin.site.register(AlertAlertRecipients)
+admin.site.register(AlertRecipients)
+admin.site.register(TargetAlert, TargetAlertAdmin)
