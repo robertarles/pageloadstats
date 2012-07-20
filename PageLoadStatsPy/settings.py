ae3b1bed971429b5ab1a26d0d3631d63eac5bd49
robert <robert@arles.us>
2012-07-20 07:19:56 -0700

    used full class name PageLoadStatsPy.pageloadstats  to include app

--- PageLoadStatsPy/settings.py
+++ PageLoadStatsPy/settings.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     #java'django.contrib.admindocs',
-    'pageloadstats',
+    'PageLoadStatsPy.pageloadstats',
 )
 
 # A sample logging configuration. The only tangible logging
