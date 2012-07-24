42eec8fa59512d6145ce7a67ca8ad438d0728e37
robert <robert@arles.us>
2012-07-24 12:12:48 -0700

    sql port to 3306 for prod

--- PageLoadStatsPy/settings.py
+++ PageLoadStatsPy/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         'USER': 'citysearch',                      # Not used with sqlite3.
         'PASSWORD': 'citysearch@@',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3366',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
