5f81aed0e66b936e44a37f0f845380efe1ab50f5
Robert Arles <robert.arles@gifts.com>
2015-01-29 08:59:58 -0800

    removing cruft, personal settings

--- PageLoadStatsPy/pageloadstats/settings.py
+++ PageLoadStatsPy/pageloadstats/settings.py
@@ -6,7 +6,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('Robert', 'robert@arles.us'),
+    ('test', 'test@test.com'),
 )
 
 MANAGERS = ADMINS
@@ -16,7 +16,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'pagestats',                      # Or path to database file if using sqlite3.
         'USER': 'pagestats',                      # Not used with sqlite3.
-        'PASSWORD': 'pagestats@@',                  # Not used with sqlite3.
+        'PASSWORD': 'somepassword',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
