d3acaee2a06d769e3666680860227a4307cddd87
Robert Arles <robert@rambp.local>
2015-07-13 10:21:59 -0700

    Removed legacy citysearch java code copy/support

--- updateCsSources.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-cp --recursive --update /home/robert/workspace/master-regression-suite/src/test/java/com/citysearch/performance/PageLoadStatsPy/PageLoadStatsJava/ PageLoadStatsJava/
-cp --recursive --update PageLoadStatsPy/ /home/robert/workspace/master-regression-suite/src/test/java/com/citysearch/performance/PageLoadStatsPy/
